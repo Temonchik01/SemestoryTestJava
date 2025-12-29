698880bc7f71b278023613f513c1f2470a63a71a
Tymofii Stirenko <98258034+Temonchik01@users.noreply.github.com>
2025-12-29 09:43:13 +0200

    Create WeatherData.java

---
+++ WeatherData.java
@@ -0,0 +1,17 @@
+public class WeatherData {
+    private final double temperature;
+    private final double humidity;
+
+    public WeatherData(double temperature, double humidity) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+}
